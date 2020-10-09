cbce0599c2356c0a9eabad0e7bc3d11e71886ad0
Stephen Helwig <stephel@microsoft.com>
2020-10-09 14:50:56 -0400

    add health page and sql connection

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCoreSqlDb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreSqlDb.Controllers
+{
+    public class HealthController : Controller
+    {
+        private readonly MyDatabaseContext _context;
+        public DbSet<string> ServerName { get; set; }
+
+        public HealthController(MyDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var healthStatus = new HealthStatus();
+
+            var dbConnection = _context.Database.GetDbConnection();
+            healthStatus.DbServerName = dbConnection.DataSource;
+
+            return View(healthStatus);
+        }
+    }
+}
